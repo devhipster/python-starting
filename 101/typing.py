16ad383412ad36cf695f9fe4f618bc34c24aa165
HipsterSantos <santoscampos269@gmail.com>
2021-04-09 23:24:44 +0100

    typing hint

---
+++ 101/typing.py
@@ -0,0 +1,12 @@
+#type hint
+""" 
+The most fundamental support consits of the types : 
+Any, Union,tuple ,Callable , TypeVar, and Generic
+
+"""
+#an example of annotation 
+
+def greeting(name: str) -> str: 
+    return 'Hello '+name
+
+    
